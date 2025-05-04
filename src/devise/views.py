5cf566bbe8f888d7e1d166ff002e6e25f0f6d2f0
Younous TCHAO <younoustchao@gmail.com>
2025-05-04 17:59:03 +0000

    Simulation de context

--- src/devise/views.py
+++ src/devise/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def dashboard(request):
-    return render(request, "devise/index.html")
+    return render(request, "devise/index.html", context={"liste_nombre": range(15)})
