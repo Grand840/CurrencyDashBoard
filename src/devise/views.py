02a40c1af2ab10199eb43d7cbb34d600467418e8
Younous TCHAO <younoustchao@gmail.com>
2025-05-17 22:15:21 +0000

    Ajout pour modif des urls

--- src/devise/views.py
+++ src/devise/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import api
 
 # Create your views here.
-def dashboard(request):
-    return render(request, "devise/index.html", context={"liste_nombre": range(15)})
+def dashboard(request, days_range=30, currencies="USD"):
+    days, rates = api.get_rates(currencies=currencies.split(","), days=days_range)
+    return render(request, "devise/index.html", context={"data":rates["USD"],
+                                                                      "days_labels":days})
