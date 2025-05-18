1a34eeb3cc26d48596c5b389c3b27c235ea1a90c
Younous TCHAO <younoustchao@gmail.com>
2025-05-18 17:54:29 +0000

    Affichage de nbre de jours

--- src/devise/views.py
+++ src/devise/views.py
@@ -5,5 +5,7 @@ import api
 # Create your views here.
 def dashboard(request, days_range=30, currencies="USD"):
     days, rates = api.get_rates(currencies=currencies.split(","), days=days_range)
+    page_label = {7: "Semaine", 30: "Mois", 365: "Année"}.get(days_range, "Personalisé")
     return render(request, "devise/index.html", context={"data":rates,
-                                                                      "days_labels":days})
+                                                                      "days_labels":days,
+                                                                      "page_label":page_label})
